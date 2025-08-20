a4fc36833ab87e0d412f1c50bfd53a2e898a169a
Yo from Olivia's PC <tanos75@free.fr>
2025-08-20 13:17:06 +0200

    use user input to display exercise specifics

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 # pylint: disable=missing-module-docstring
 
+import ast
 import duckdb
 import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
-
-# ANSWER_STR = """
-# SELECT * FROM beverages
-# CROSS JOIN food_items
-# """
-# solution_df = duckdb.sql(ANSWER_STR).df()
-
-
-
 
 with st.sidebar:
     theme = st.selectbox(
@@ -23,16 +15,16 @@ with st.sidebar:
     )
     st.write(f"You have selected {theme}")
 
-    exercice = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
-    st.write(exercice)
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercise)
 
 st.header("Enter your code :")
 query = st.text_area(label="Enter SQL request : ", key="user_input")
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
-#
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
+
 #     # Column checks
 #     try:
 #         result = result[solution_df.columns]
@@ -46,15 +38,17 @@ query = st.text_area(label="Enter SQL request : ", key="user_input")
 #         st.write("Some lines are missing")
 #
 #
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#     st.dataframe(solution_df)
-#
-# with tab3:
-#     st.write(ANSWER_STR)
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+     exercise_tables = exercise.loc[0, "tables"]
+     for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    st.write(answer)
